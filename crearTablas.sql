ce6c932f8b7b38a0c0a650b1e8eefb301556f344
Pablo Gonzalez <pagoncastro@gmail.com>
2019-12-07 21:05:46 -0400

    Agregar atributo a categorias. Arreglar referencias

--- crearTablas.sql
+++ crearTablas.sql
@@ -8,11 +8,11 @@ CREATE TABLE Usuarios (
 	metodoP VARCHAR(32) NOT NULL
 );
 
-
 CREATE TABLE Categorias(
 	id SERIAL PRIMARY KEY,
 	nombre VARCHAR(32) NOT NULL,
-	parent VARCHAR(32)
+	parent_id INT4 REFERENCES Categorias(id),
+	es_hoja BOOLEAN
 );
 
 CREATE TABLE Productos(
@@ -31,12 +31,34 @@ CREATE TABLE Especificaciones(
 
 CREATE TABLE Subastas_activas (
 	id SERIAL PRIMARY KEY,
-	producto_id SERIAL REFERENCES Productos(id) NOT NULL,
+	producto_id INT4 REFERENCES Productos(id) NOT NULL,
 	descripcion TEXT,
-	especificacion_id SERIAL REFERENCES Especificaciones(id),
+	especificacion_id INT4 REFERENCES Especificaciones(id),
 	precio_base FLOAT8,
 	precio_reserva FLOAT8,
 	precio_actual FLOAT8
 );
 
-INSERT INTO Usuarios()
+-- Insert de Usuarios
+INSERT INTO Usuarios(nombre, metodoP)
+VALUES("Edith", "Credito");
+INSERT INTO Usuarios(nombre, metodoP)
+VALUES("Vivien", "Transferencia");
+INSERT INTO Usuarios(nombre, metodoP)
+VALUES("Daniel", "Crypto");
+INSERT INTO Usuarios(nombre, metodoP)
+VALUES("Laurella", "Debito");
+INSERT INTO Usuarios(nombre, metodoP)
+VALUES("Osbert", "Crypto");
+INSERT INTO Usuarios(nombre, metodoP)
+VALUES("Sebastian", "Debito");
+INSERT INTO Usuarios(nombre, metodoP)
+VALUES("Cleopatra", "Crédito");
+INSERT INTO Usuarios(nombre, metodoP)
+VALUES("Doy", "Transferencia");
+INSERT INTO Usuarios(nombre, metodoP)
+VALUES("Anna", "Crédito");
+INSERT INTO Usuarios(nombre, metodoP)
+VALUES("Diana", "Transferencia");
+
+-- Insert de Categorias
