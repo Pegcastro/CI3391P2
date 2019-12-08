3b7e92752cb60061248b36be79d46b57945ef337
Pablo Gonzalez <pagoncastro@gmail.com>
2019-12-07 20:32:05 -0400

    Arreglado problema de creacion de relaciones

--- crearTablas.sql
+++ crearTablas.sql
@@ -8,21 +8,6 @@ CREATE TABLE Usuarios (
 	metodoP VARCHAR(32) NOT NULL
 );
 
-CREATE TABLE Subastas_activas (
-	id INT4 PRIMARY KEY,
-	nombre VARCHAR(32) REFERENCES Productos(nombre) NOT NULL,
-	descripcion TEXT,
-	especificacion TEXT REFERENCES Especificaciones(valor),
-	precio_base FLOAT8,
-	precio_reserva FLOAT8,
-	precio_actual FLOAT8
-);
-
-CREATE TABLE Productos(
-	id INT4 PRIMARY KEY,
-	nombre VARCHAR(32) NOT NULL,
-	categoria VARCHAR(32) REFERENCES Categorias(nombre) NOT NULL
-);
 
 CREATE TABLE Categorias(
 	id INT4 PRIMARY KEY,
@@ -30,9 +15,26 @@ CREATE TABLE Categorias(
 	parent VARCHAR(32)
 );
 
+CREATE TABLE Productos(
+	id INT4 PRIMARY KEY,
+	nombre VARCHAR(32) NOT NULL,
+	categoria INT4 REFERENCES Categorias(id) NOT NULL
+);
+
+
 CREATE TABLE Especificaciones(
 	id INT4 PRIMARY KEY,
 	producto_id INT4 REFERENCES Productos(id),
 	nombre VARCHAR(32),
 	valor VARCHAR(50)
+);
+
+CREATE TABLE Subastas_activas (
+	id INT4 PRIMARY KEY,
+	nombre INT4 REFERENCES Productos(id) NOT NULL,
+	descripcion TEXT,
+	especificacion INT4 REFERENCES Especificaciones(id),
+	precio_base FLOAT8,
+	precio_reserva FLOAT8,
+	precio_actual FLOAT8
 );
